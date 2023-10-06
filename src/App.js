ad348f3c4bee5eec3ea5bac5ac8fc4c3aa0a00ab
thisjunghyewon <this.junghyewon@gmail.com>
2023-10-06 15:20:09 +0200

    Setting up ScatterChart

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
